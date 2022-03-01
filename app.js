727aad7c576005cd442029fee99acab125261a12
youguifang <guifang.you@tcl.com>
2022-03-01 14:10:30 +0800

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pageinnerRouter = require('./routes/pageinner');
+var testRouter = require('./routes/testimgs2pdf2word');
 
 var app = express();
 
@@ -16,13 +17,16 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pageinner', pageinnerRouter)
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
